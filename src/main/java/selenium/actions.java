78495ab88a76c6f91906fb666f9208c0d65a74de
Kavippiriyan <138883580+Kavippiriyan@users.noreply.github.com>
2023-07-11 12:52:01 +0530

    actions commit

---
+++ src/main/java/selenium/actions.java
@@ -0,0 +1,31 @@
+package selenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class actions {
+
+	public static void main(String[] args) {
+		WebDriver d = new ChromeDriver();
+		Actions a= new Actions(d);
+		
+		d.get("https://demoqa.com/droppable/");
+		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		d.manage().window().maximize();
+		
+		By drag = By.xpath("//div[@id='draggable']");
+		WebElement click_drag = d.findElement(drag);
+		
+		By drop = By.xpath("//div[@id='droppable']//p[text()='Drop here']");
+		WebElement click_drop = d.findElement(drop);
+		
+		a.dragAndDrop(click_drag, click_drop).perform();
+
+	}
+
+}
