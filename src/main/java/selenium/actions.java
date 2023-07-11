8658d6a6dea4acf1e5c60a0c68d26607964c27db
Kavippiriyan <138883580+Kavippiriyan@users.noreply.github.com>
2023-07-11 17:00:58 +0530

    Drag and Drop
    Drag and Drop

--- src/main/java/selenium/actions.java
+++ src/main/java/selenium/actions.java
@@ -13,24 +13,19 @@ public class actions {
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver d = new ChromeDriver();
 		Actions a= new Actions(d);
-		
+
 		d.get("https://demoqa.com/droppable/");
 		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 		d.manage().window().maximize();
-		
-		Thread.sleep(2000);
-		By drag = By.xpath("//div[@id='draggable']");
-		WebElement click_drag = d.findElement(drag);
-		Thread.sleep(2000);
-		
-		Thread.sleep(2000);
-		By drop = By.xpath("//div[@id='droppable']//p[text()='Drop here']");
-		WebElement click_drop = d.findElement(drop);
-		Thread.sleep(2000);
-		
-		Thread.sleep(2000);
-		a.dragAndDrop(click_drag, click_drop).perform();
 
+
+		By drag=By.xpath("//div[@id=\"draggable\"]");
+		WebElement drag_button=d.findElement(drag);
+
+		By drop=By.xpath("//p[text()='Dropped!']");
+		WebElement drop_button=d.findElement(drop);
+
+		a.dragAndDrop(drag_button, drop_button).build().perform();
 	}
 
 }
