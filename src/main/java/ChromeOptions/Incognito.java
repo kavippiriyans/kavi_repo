5ed29271937d453b68e6607b31fa6ef40e1aa517
Kavippiriyan <138883580+Kavippiriyan@users.noreply.github.com>
2023-07-25 11:10:08 +0530

    Chromeoptions---Incognito
    Chromeoptions---Incognito

---
+++ src/main/java/ChromeOptions/Incognito.java
@@ -0,0 +1,22 @@
+package ChromeOptions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Incognito 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		ChromeOptions options=new ChromeOptions();
+		options.addArguments("--incognito","--start-maximized");
+		WebDriver driver=new ChromeDriver(options);
+		
+		driver.get("https://www.google.com");
+		
+
+	}
+
+}
